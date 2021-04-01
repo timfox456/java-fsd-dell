259691cec032f2b6fe00f715a47ba64e197b5e82
Tim Fox <tfox@createksolutions.com>
2021-04-01 15:27:04 -0400

    Added thymeleaf template hello and MainController

---
+++ SpringStarter/src/main/java/com/example/SpringStarter/MainController.java
@@ -0,0 +1,17 @@
+package com.example.SpringStarter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String printHello() 
+	{
+		return("hello");
+	}
+	
+	
+}
